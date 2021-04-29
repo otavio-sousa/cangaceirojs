89b945915cfc643126485a494b72f48e3b4127e8
otavio-sousa <otavioklearskin@gmail.com>
2021-04-29 20:04:46 -0300

    Congelando as propriedades de um objeto e tratando de programacao defensiva

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -6,24 +6,28 @@ class Negociacao {
     // definir um underline à propriedade indica que esta não deva ser alterada (somente pelos métodos de dentro da própria class)
     constructor(data, quantidade, valor){
 
-        this._data = data,
+        this._data = new Date(data.getTime()), // programacao defensiva
         this._quantidade = quantidade,
-        this._valor = valor
+        this._valor = valor,
+        Object.freeze(this) // congela as propriedades do objeto
     }
 
     // definimos os métodos diretos na classe
     // esses métodos são comportamentos que damos aos nossos dados
+
+    // métodos acessadores
+    // servem para acessarmos os dados da class
+    // o prefixo get é utilizado para acessarmos os valores como se fosse o acesso direto à propriedade -> negociacao.quantidade
     get volume(){
 
         return this._quantidade * this._valor
     }
 
-    // métodos acessadores
-    // servem para acessarmos os dados da class
-    // o prefixo get é utilizado para acessarmos os valores como se fosse o acesso direto à propriedade -> negociacao.quantidade
     get data(){
 
-        return this._data
+        // Programação defensiva
+        // criar um novo objeto de data impede que o objeto retornado da propriedade data tenha seus métodos alterados
+        return new Date(this._data.getTime())
     }
 
     get quantidade(){
