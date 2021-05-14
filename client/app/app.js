fa0b413ccc4968b37544dbc1dc4f0055a7f99dbf
otavio-sousa <otavioklearskin@gmail.com>
2021-05-14 19:40:33 -0300

    Capitulo 7: Fazendo uso de declaracao de variaveis com const

--- client/app/app.js
+++ client/app/app.js
@@ -1,8 +1,8 @@
 // o app.js inicia nossa aplicão
 
-var negociacaoController = new NegociacaoController()
+const negociacaoController = new NegociacaoController()
 
-var formulario = document.querySelector('form')
+const formulario = document.querySelector('form')
 
 // formulario.addEventListener('submit', function(event){
 //     negociacaoController.adiciona(event)
